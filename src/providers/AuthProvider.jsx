340f3f2a71649ce5ba95405a24a1692092d3d374
fahimrashid3 <fahimrashid350@gmail.com>
2025-11-06 00:31:14 +0600

    Refactor Firebase configuration by moving initialization from Firebase.config.js to AuthProvider.jsx. This change centralizes the Firebase setup within the AuthProvider, ensuring better integration with the authentication context.

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -12,7 +12,18 @@ import {
 } from "firebase/auth";
 
 import useAxiosPublic from "../hooks/useAxiosPublic";
-import { app } from "../firebase/firebase.config";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+};
+
+export const app = initializeApp(firebaseConfig);
 
 export const authContext = createContext(null);
 
